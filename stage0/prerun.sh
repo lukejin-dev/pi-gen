112d9848fb0977ae87f10b667666ab7cbacaceb3
Lu Ken <ken.lu@intel.com>
2020-09-09 20:31:35 +0800

    use chinese mirror for raspberry packages

--- stage0/prerun.sh
+++ stage0/prerun.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
 if [ ! -d "${ROOTFS_DIR}" ]; then
-	bootstrap ${RELEASE} "${ROOTFS_DIR}" http://raspbian.raspberrypi.org/raspbian/
+	#bootstrap ${RELEASE} "${ROOTFS_DIR}" http://raspbian.raspberrypi.org/raspbian/
+	bootstrap ${RELEASE} "${ROOTFS_DIR}" http://mirrors.nju.edu.cn/raspbian/raspbian/
 fi
