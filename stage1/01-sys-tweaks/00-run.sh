1e20d2f7c99255f77e4c49c9ca71b2133638b89a
Lu Ken <ken.lu@intel.com>
2020-09-10 10:00:35 +0800

    add motd

--- stage1/01-sys-tweaks/00-run.sh
+++ stage1/01-sys-tweaks/00-run.sh
@@ -3,6 +3,7 @@
 install -d "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d"
 install -m 644 files/noclear.conf "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d/noclear.conf"
 install -v -m 644 files/fstab "${ROOTFS_DIR}/etc/fstab"
+install -v -m 644 files/motd "${ROOTFS_DIR}/etc/motd"
 
 on_chroot << EOF
 if ! id -u ${FIRST_USER_NAME} >/dev/null 2>&1; then
