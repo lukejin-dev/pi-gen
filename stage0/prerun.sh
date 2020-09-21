3f94bf3a5755e86fdc2fee6363b44ad1619d8219
Lu Ken <ken.lu@intel.com>
2020-09-21 21:07:55 +0800

    use ustc mirror

--- stage0/prerun.sh
+++ stage0/prerun.sh
@@ -2,5 +2,5 @@
 
 if [ ! -d "${ROOTFS_DIR}" ]; then
 	#bootstrap ${RELEASE} "${ROOTFS_DIR}" http://raspbian.raspberrypi.org/raspbian/
-	bootstrap ${RELEASE} "${ROOTFS_DIR}" http://mirrors.zju.edu.cn/raspbian/raspbian/
+	bootstrap ${RELEASE} "${ROOTFS_DIR}" http://mirrors.ustc.edu.cn/raspbian/raspbian
 fi
